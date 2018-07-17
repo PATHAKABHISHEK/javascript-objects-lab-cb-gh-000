c2e0bb392817455599afb70ff503576f0f3d3bbe
Abhishek Pathak <pathakabhishek0003@gmail.com>
2018-07-17 12:58:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var recipes = new Object()
-
+var new_object
 var updateObjectWithKeyAndValue = (object,key,value) =>{
   object[key] = value
   return object
 
-}
+} 
 
 var updateObjectWithKeyAndValue = (object,key,value) =>{
    new_object = Object.assign(object,{key:value})
   return new_object
 
-}
+} 
