b8ed3e04d735233196693a3a31109e65565f498c
Abhishek Pathak <pathakabhishek0003@gmail.com>
2018-07-17 12:57:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ var updateObjectWithKeyAndValue = (object,key,value) =>{
   object[key] = value
   return object
 
-} 
+}
 
 var updateObjectWithKeyAndValue = (object,key,value) =>{
    new_object = Object.assign(object,{key:value})
   return new_object
 
-} 
+}
